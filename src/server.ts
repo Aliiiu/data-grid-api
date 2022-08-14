7f242cf680a88d65513dffae47554d06eef9e810
aliiiu <ayokunumi99@gmail.com>
2022-08-14 09:24:08 +0100

    ready to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express, { Application } from 'express';
 import './utils/db-connector';
 import { router } from './routes/employee.route';
-import cors from 'cors';
+// import cors from 'cors';
+const cors = require('cors');
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
